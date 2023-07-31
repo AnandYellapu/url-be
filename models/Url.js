599bfc9d1a281b99f07ace0c3c92a279eeab0a98
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2023-07-31 08:14:23 +0530

    Update Url.js

--- models/Url.js
+++ models/Url.js
@@ -1,22 +1,39 @@
+// const mongoose = require('mongoose');
+
+// const urlSchema = new mongoose.Schema(
+//     {
+//     shortURL: String,
+//   longURL: String,
+//     clickCount: {
+//       type: Number, default: 0
+//     },
+//   _id: {
+//     type: Date,
+//     default: Date.now,
+//     },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// const Url = mongoose.model('Url', urlSchema);
+
+// module.exports = Url;
+
+
+
 const mongoose = require('mongoose');
 
-const urlSchema = new mongoose.Schema(
-    {
-    shortURL: String,
-  longURL: String,
-    clickCount: {
-      type: Number, default: 0
-    },
-  _id: {
-    type: Date,
-    default: Date.now,
-    },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+const urlSchema = new mongoose.Schema({
+  longURL: { type: String, required: true },
+  shortURL: { type: String, required: true },
+  clickCount: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Url = mongoose.model('Url', urlSchema);
 
 module.exports = Url;
+
+
